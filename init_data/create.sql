a2da2227801cda7f63561e816bc1934dbecb60b0
Cole K <ckrant4@gmail.com>
2022-11-07 23:22:47 -0700

    back end work

--- init_data/create.sql
+++ init_data/create.sql
@@ -1,29 +1,29 @@
 CREATE TABLE users(
-	username VARCHAR(50) PRIMARY KEY,
+	username VARCHAR(50) PRIMARY KEY NOT NULL,
 	password CHAR(60) NOT NULL
 );
 
 CREATE TABLE fitness(
 	fitness_id INT PRIMARY KEY,
-	day DATE,
-	muscle TEXT,
-	exercise TEXT,
-	sets INT,
-	reps INT,
-	weight FLOAT
+	day DATE NOT NULL,
+	muscle TEXT NOT NULL,
+	exercise TEXT NOT NULL,
+	sets INT NOT NULL,
+	reps INT NOT NULL,
+	weight FLOAT NOT NULL
 );
 
 CREATE TABLE nutrition(
 	nutrition_id INT PRIMARY KEY,
-	day DATE,
-	meal TEXT,
+	day DATE NOT NULL,
+	meal TEXT NOT NULL,
 	calories INT
 );
 
 CREATE TABLE water(
 	water_id INT PRIMARY KEY,
-	day DATE,
-	milliliters INT
+	day DATE NOT NULL,
+	milliliters INT NOT NULL
 );
 
 CREATE TABLE user_fitness(
@@ -40,3 +40,17 @@ CREATE TABLE user_water(
 	username VARCHAR(50) REFERENCES users(username),
 	water_id INT NOT NULL REFERENCES water(water_id)
 );
+
+
+-- -- Views to simplify queires in the server --
+-- CREATE OR REPLACE VIEW 
+-- 	Fitness_Today AS
+-- SELECT
+-- 	day,
+-- 	exercise,
+-- 	sets,
+-- 	reps,
+-- 	weight
+-- FROM
+-- 	fitness
+-- WHERE day = GETDATE() AS DATE;
