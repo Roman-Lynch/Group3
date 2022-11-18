2c97fb976e2007752a3b7258f042b1cf848e2395
Cole K <ckrant4@gmail.com>
2022-11-17 21:18:15 -0700

    Worked on routing exercisehistory

--- index.js
+++ index.js
@@ -298,6 +298,35 @@ app.put('/edit_workout', (req, res) => {
 });
 
 /* DELETE EXERCISE ------------------------------------------------------------ */
+// app.delete("/delete_exercise", (req, res) => {
+//     const query = 
+//     `DELETE FROM
+//         fitness
+//      WHERE
+//         day = $1
+//         AND muscle = $2
+//         AND exercise = $3
+//         AND weight = $4
+//         AND sets = $5
+//         AND reps = $6;`;
+
+//     db.any(query, [req.body.day, req.body.muscle, req.body.exercise, req.body.weight, req.body.sets, req.body.reps])
+//         .then((rows) => {
+//             res.render("pages/exercisehistory", { 
+//                 username: req.session.user.username, 
+//                 fitness: rows,
+//                 action: "delete",
+//                 });
+//         })
+//         .catch((err) => {
+//             res.render("pages/exercisehistory", {
+//                 username: req.session.user.username,
+//                 fitness: [],
+//                 error: true,
+//                 message: err.message,
+//             });
+//         });
+// });
 app.post("/delete_exercise", (req, res) => {
     db.task("delete-exercise", (task) => {
       return task.batch([
@@ -333,5 +362,5 @@ app.post("/delete_exercise", (req, res) => {
         });
       });
   });
-/* ------------------------------------------------------------------------------------ */
+// /* ------------------------------------------------------------------------------------ */
 app.listen(3000);
